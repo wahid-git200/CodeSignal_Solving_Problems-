31071f830d20d5d6277d7d6559b1ca4919d10cb9
Abdulwahid Najafi <147462000+wahid-git200@users.noreply.github.com>
2024-01-17 14:49:00 +0430

    Add files via upload

---
+++ Add.java
@@ -0,0 +1,21 @@
+ // link of tis broblem pist this on your browser
+//https://www.codechef.com/problems/BULLET
+
+
+
+
+class Add
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		int a = 10;
+		int b = 20;
+		System.out.println(solution(a,b));
+
+	}
+	//this is unswer
+	static int solution(int param1, int param2) {
+     return param1+param2;
+    }
+
+}
